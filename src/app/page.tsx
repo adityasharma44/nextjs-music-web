9c1a4a86bf4dbae11957d51ba6b2c5669914768a
Aditya Sharma <as7007731@gmail.com>
2025-01-22 15:37:59 +0530

    homepage code 22 jan 2025

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
+
+export default function Home() {
+  return (
+    <div className="">
+      <HeroSection />
+      <FeaturedCourses/>
+      <WhyChooseUs/>
+      <TestimonialCards/>
+      <UpcomingWebinars/>
+      <Instructors/>
+      <Footer/>
+    </div>
+  );
+}
